e4955b11d19ac030ad3e481d5f471c23de9d1e7d
Qb1005 <104556791+Qb1005@users.noreply.github.com>
2026-09-13 16:56:19 +0700

    Change Environment Role and Improve connection checker

--- src/check_snowflake_connection.py
+++ src/check_snowflake_connection.py
@@ -4,31 +4,37 @@ import snowflake.connector
 
 
 def check_connection():
-
     conn = snowflake.connector.connect(
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        user=os.getenv("SNOWFLAKE_USER"),
-        password=os.getenv("SNOWFLAKE_PASSWORD"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database=os.getenv("SNOWFLAKE_DATABASE"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA"),
-        role=os.getenv("SNOWFLAKE_ROLE"),
+        account=os.environ["SNOWFLAKE_ACCOUNT"],
+        user=os.environ["SNOWFLAKE_USER"],
+        password=os.environ["SNOWFLAKE_PASSWORD"],
+        warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
+        database=os.environ["SNOWFLAKE_DATABASE"],
+        schema=os.environ["SNOWFLAKE_SCHEMA"],
+        role=os.environ["SNOWFLAKE_ROLE"],
     )
 
     cursor = conn.cursor()
 
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT
             CURRENT_USER(),
             CURRENT_ROLE(),
             CURRENT_DATABASE(),
             CURRENT_SCHEMA(),
             CURRENT_WAREHOUSE()
-    """)
+        """
+    )
 
     result = cursor.fetchone()
 
-    print(result)
+    print("Snowflake session:")
+    print(f"User:      {result[0]}")
+    print(f"Role:      {result[1]}")
+    print(f"Database:  {result[2]}")
+    print(f"Schema:    {result[3]}")
+    print(f"Warehouse: {result[4]}")
 
     cursor.close()
     conn.close()
